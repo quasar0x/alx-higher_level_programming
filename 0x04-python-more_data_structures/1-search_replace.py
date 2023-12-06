8024762b646b9aa3df4183e31de21efae32ebcbf
quasar0x <dizevbije94@gmail.com>
2023-12-06 23:51:04 +0000

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
-    
+    return [replace if search == n else n for n in my_list]
